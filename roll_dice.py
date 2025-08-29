1150b76c0a7900cfe5da9d24de7f16e9cc5cb5f8
Pranavpathe <144332103+Pranavpathe@users.noreply.github.com>
2025-08-29 13:36:54 +0530

    Create roll_dice.py

---
+++ roll_dice.py
@@ -0,0 +1,14 @@
+import random
+while True:
+    choice=input("Roll The Dice 🎲: (y/n):").lower()
+    if choice=='y':
+        die1=random.randint(1,6)
+        die2=random.randint(1,6)
+        print(f"{die1},{die2}")
+    elif choice=='n':
+        print("Thanks For Playing!")
+        break
+    else:
+        print("Please Enter Valid Input!!")
+    
+
